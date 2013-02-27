033c9b0afb31e708a8c6f8c8abab4f2d979536dc
romy-khetan <romy_khetan@persistent.co.in>
2013-02-27 16:34:53 +0530

    fresh commit

---
+++ User.java
@@ -0,0 +1,26 @@
+package org.examples.model;
+
+public class User {
+	int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	String first;
+	String last;
+
+}
