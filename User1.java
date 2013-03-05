e5509567bd39a823128d13c37c0ec97fc9364d36
romy-khetan <romy_khetan@persistent.co.in>
2013-03-05 18:37:23 +0530

    newest

---
+++ User1.java
@@ -0,0 +1,74 @@
+package org.examples.model;
+
+public class User {
+	int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+///////////////////////////////////////////////
+public void setFirst(String first) {
+		this.first = first;
+	}
+
+
+////////////////////////////////////////////////
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+
+
+
+////////////////////////
+
+
+
+my 3rd change
+
+
+//////////////
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+
+///////////////
+
+my 3r changeeeeeeeeee
+
+/////////
+
+
+
+
+	String last;
+
+////////////////////////////////////////////////////////////
+
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	String first;
+	String last;
+
+\\\\\\\\\\\\\\\\\\\
+
+
+4th change
+
+\\\\\\\\
+
+
+
+}
